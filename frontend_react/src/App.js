47e05ac80ec4beb262b19a31d0a56191c958b6cc
James Xu <jamesxu_1996@hotmail.com>
2022-06-16 05:19:59 +1000

    folder restructuring for container components

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const app = () => {
-	return <h1>App</h1>;
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
+const App = () => {
+	return (
+		<div className="app">
+			<Navbar>fdsaf</Navbar>
+			<Header>fdafdsf</Header>
+			<About></About>
+			<Work></Work>
+			<Skills></Skills>
+			<Testimonial></Testimonial>
+			<Footer></Footer>
+		</div>
+	);
 };
 
-export default app;
+export default App;
